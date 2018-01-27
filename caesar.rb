7186b3db88448f79d2c1d9ea100fb50002c10e6c
grimyoung <samhyukim@gmail.com>
2018-01-27 14:14:01 -0800

    caesar.rb now contains the cipher method

--- caesar.rb
+++ caesar.rb
@@ -1,5 +1,15 @@
-require "sinatra"
+#works so long as ascii encoding doesn't change
 
-get "/" do 
-  "Hello World!"
+def caesar(string = "", shift = 0)
+	cipher =""
+	string.scan(/./) do |letter|
+		num = letter.ord
+		if num >= 65 && num <=90
+			num = (num-65 + (shift.to_i))%26 + 65
+		elsif num >= 97 && num <=122
+			num = (num-97 + (shift.to_i))%26 + 97
+		end
+		cipher += num.chr
+	end
+	cipher
 end
